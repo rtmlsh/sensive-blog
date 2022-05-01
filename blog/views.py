17f6a463fdb7be1951c4ca6751d013ae2b50687a
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-05-01 23:11:14 +0300

    Move code to reusable QuerySet method

--- blog/views.py
+++ blog/views.py
@@ -7,18 +7,18 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-# def serialize_post_optimized(post):
-#     return {
-#         'title': post.title,
-#         'teaser_text': post.text[:200],
-#         'author': post.author.username,
-#         'comments_amount': len(Comment.objects.filter(post=post)),
-#         'image_url': post.image.url if post.image else None,
-#         'published_at': post.published_at,
-#         'slug': post.slug,
-#         'tags': [serialize_tag(tag) for tag in post.tags.all()],
-#         'first_tag_title': post.tags.all()[0].title,
-#     }
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
 
 def serialize_post(post):
@@ -26,7 +26,7 @@ def serialize_post(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': post.comments_count,
+        'comments_amount': len(Comment.objects.filter(post=post)),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -63,9 +63,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts[:5]
+            serialize_post_optimized(post) for post in most_popular_posts[:5]
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts[:5]],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts[:5]],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)

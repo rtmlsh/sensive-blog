7356910d31423d3b01315628865affa6613d3a37
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-05-16 22:15:53 +0300

    Reduce comments authors related posts

--- blog/views.py
+++ blog/views.py
@@ -110,6 +110,7 @@ def tag_filter(request, tag_title):
         .fetch_with_comments_count()
 
     related_posts = tag.posts.all()\
+        .prefetch_related('author')\
         .prefetch_posts_count_for_tags()\
         .fetch_with_comments_count()[:20]
 
